e590e647b50e8d64b7eb0b6f999672c0628d6fc9
Veeraraghavan <virunarasimhan25@gmail.com>
2021-05-28 18:19:05 +0530

    initial commit

---
+++ game/main.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+#include <string>
+#include <cmath>
+
+//Screen dimension constants
+const int SCREEN_WIDTH = 1280;
+const int SCREEN_HEIGHT = 720;
+
